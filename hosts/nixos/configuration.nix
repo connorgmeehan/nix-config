630a34a44e3584edb1d819ed311224c4400db955
Connor Meehan <connorgm@pm.me>
2023-06-30 10:50:34 +1000

    fix(nixos): Fixes SSH Config

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -198,9 +198,9 @@
   services.openssh = {
     enable = true;
     # Forbid root login through SSH.
-    permitRootLogin = "no";
+    settings.PermitRootLogin = "no";
     # Use keys only. Remove if you want to SSH using password (not recommended)
-    passwordAuthentication = false;
+    settings.PasswordAuthentication = false;
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
