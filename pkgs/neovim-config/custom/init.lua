57d2fa5a07d4a6522dd68f3729e5f39728e27a2b
Connor G Meehan <connorgm@pm.me>
2023-07-13 13:42:54 +1000

    feat(nvim): Attempted treesitter fold fix

--- pkgs/neovim-config/custom/init.lua
+++ pkgs/neovim-config/custom/init.lua
@@ -6,3 +6,12 @@
 --   command = "tabdo wincmd =",
 -- })
 vim.opt.relativenumber = true
+vim.opt.foldlevel = 20
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- Fix for https://github.com/nvim-telescope/telescope.nvim/issues/699
+-- Files opened with telescope wont fold.
+vim.api.nvim_create_autocmd({ "BufEnter", "BufNew", "BufWinEnter"  }, {
+  group = vim.api.nvim_create_augroup("ts_fold_workaround", { clear = true }),
+  command = "set foldexpr=nvim_treesitter#foldexpr()",
+})
