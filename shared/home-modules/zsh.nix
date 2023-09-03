2508ca3bc29b6ddd06afa7e4e191133ee0047f84
Connor Meehan <connorgm@pm.me>
2023-09-03 18:18:59 +1000

    feat(zsh): Adds dir-env + nix integration

--- shared/home-modules/zsh.nix
+++ shared/home-modules/zsh.nix
@@ -5,51 +5,57 @@ let
 
   iceToStr = ices: "zinit ice ${concatStrings (map (ice: " ${ice}") ices)}";
 in {
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      ns = "nix-shell --command zsh";
-      ll = "ls -la";
+    programs.direnv = {
+          enable = true;
+          enableZshIntegration = true; # see note on other shells below
+          nix-direnv.enable = true;
     };
-    initExtraFirst = ''
-      source ~/.p10k.zsh
 
-    '';
-    initExtra = ''
-      # Netlify Large media
-      if [ -f '${config.home.homeDirectory}/.config/netlify/helper/git-config' ]; then source '${config.home.homeDirectory}/.config/netlify/helper/git-config'; fi
+    programs.zsh = {
+        enable = true;
+        shellAliases = {
+              ns = "nix-shell --command zsh";
+              ll = "ls -la";
+        };
+        initExtraFirst = ''
+              source ~/.p10k.zsh
+        '';
 
-      eval "$(fnm env --use-on-cd)"
-      export PATH="$PATH":${config.home.homeDirectory}/.cargo/bin
+        enableCompletion = false;
+        initExtraBeforeCompInit = ''
+              export ZINIT_HOME=${cfg.zinitDir}
+              source "${cfg.zinitDir}/zinit.zsh"
 
-      # PNPM setup
-      export PATH="$PATH":${config.home.homeDirectory}/.local/share/pnpm/
-      export PNPM_HOME="${config.home.homeDirectory}/.local/share/pnpm/"
-    '';
+              ${optionalString (cfg.plugins != [ ]) ''
+                    ${concatStrings (map (plugin: '' 
+                          ${ optionalString (plugin.ice != [ ]) (iceToStr plugin.ice) }
+                          zinit ${plugin.strategy} ${plugin.name}
+                    '') cfg.plugins)}
+              ''}
+        '';
 
+        initExtra = ''
+              # Netlify Large media
+              if [ -f '${config.home.homeDirectory}/.config/netlify/helper/git-config' ]; then source '${config.home.homeDirectory}/.config/netlify/helper/git-config'; fi
 
-    enableCompletion = false;
-    initExtraBeforeCompInit = ''
-      export ZINIT_HOME=${cfg.zinitDir}
-      source "${cfg.zinitDir}/zinit.zsh"
+              eval "$(fnm env --use-on-cd)"
+              export PATH="$PATH":${config.home.homeDirectory}/.cargo/bin
 
-      ${optionalString (cfg.plugins != [ ]) ''
-        ${concatStrings (map (plugin: '' 
-          ${ optionalString (plugin.ice != [ ]) (iceToStr plugin.ice) }
-          zinit ${plugin.strategy} ${plugin.name}
-        '') cfg.plugins)}
-      ''}
-    '';
-    zinit = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; ice = [ "wait'1'" "lucid" ]; } # Simple plugin installation
-        { name = "zsh-users/zsh-syntax-highlighting"; ice = [ "wait'1'" "lucid" ]; } # Simple plugin installation
-        { name = "chisui/zsh-nix-shell"; } # Simple plugin installation
-        { name = "sindresorhus/pure"; ice = [ "compile'(pure|async).zsh'" "pick'async.zsh'" "src'pure.zsh'" ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-        #{ name = "romkatv/powerlevel10k"; ice = [ "depth=1" ]; strategy = "light"; } # Installations with additional options. For the list of options, please refer to Zplug README.
-      ];
+              # PNPM setup
+              export PATH="$PATH":${config.home.homeDirectory}/.local/share/pnpm/
+              export PNPM_HOME="${config.home.homeDirectory}/.local/share/pnpm/"
+        '';
+
+        zinit = {
+              enable = true;
+              plugins = [
+                    { name = "zsh-users/zsh-autosuggestions"; ice = [ "wait'1'" "lucid" ]; } # Simple plugin installation
+                    { name = "zsh-users/zsh-syntax-highlighting"; ice = [ "wait'1'" "lucid" ]; } # Simple plugin installation
+                    { name = "chisui/zsh-nix-shell"; } # Simple plugin installation
+                    { name = "sindresorhus/pure"; ice = [ "compile'(pure|async).zsh'" "pick'async.zsh'" "src'pure.zsh'" ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+                    #{ name = "romkatv/powerlevel10k"; ice = [ "depth=1" ]; strategy = "light"; } # Installations with additional options. For the list of options, please refer to Zplug README.
+              ];
+        };
     };
-  };
 }
 
