b109393a1809225f64c5a7008552a495278a2934
Connor Meehan <connorgm@pm.me>
2023-09-03 18:11:32 +1000

    tweak(neovim): Set tab size to 4 space

--- pkgs/neovim-config/custom/init.lua
+++ pkgs/neovim-config/custom/init.lua
@@ -10,6 +10,12 @@ vim.opt.foldlevel = 20
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
+-- Indent 4 spaces
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 vim.opt.synmaxcol = 500;
 vim.opt.wrap = false;
 
