2ecbbe5f0a65a6cb8dfaed67ca4302b50425db57
Connor Meehan <connorgm@pm.me>
2024-04-20 11:19:56 +1000

    fix(nixos): re-enable x11

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -108,7 +108,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  # services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.displayManager.gdm.wayland = false;
 
   # Configure keymap in X11
   services.xserver = {
