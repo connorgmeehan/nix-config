37d18bab29435c588ae0d8786cad5d628345b823
Connor Meehan <connorgm@pm.me>
2024-04-20 13:10:32 +1000

    feat(neovim): Updates to nvchad 2.5, adds lsp arguments helper

--- pkgs/neovim-config/custom/chadrc.lua
+++ pkgs/neovim-config/custom/chadrc.lua
@@ -2,19 +2,20 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "kanagawa",
   theme_toggle = { "kanagawa", "one_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+  hl_override = {
+    Comment = {
+      italic = true,
+    },
+  },
+  hl_add = {
+    NvimTreeOpenedFolderName = { fg = "green", bold = true },
+  },
 }
 
-M.plugins = "custom.plugins"
-
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
-
 return M
--- pkgs/neovim-config/custom/configs/lspconfig.lua
+++ pkgs/neovim-config/custom/configs/lspconfig.lua
@@ -1,5 +1,6 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
@@ -33,6 +34,7 @@ for _, lsp in ipairs(servers) do
 
   print("Configuring LSP " .. lsp)
   local config = {
+    on_init = on_init,
     on_attach = on_attach,
     capabilities = capabilities,
   }
