0dfb67baade99a84c20ea0eaf017d9adb6cb102a
Connor G Meehan <connorgm@pm.me>
2024-05-20 12:51:19 +1000

    feat(zinit): Configures new zinit options

--- shared/home-modules/zsh.nix
+++ shared/home-modules/zsh.nix
@@ -15,6 +15,10 @@ in {
           nix-direnv.enable = true;
     };
 
+    home.packages = with pkgs; [
+        fnm
+    ];
+
     programs.zsh = {
         enable = true;
         shellAliases = {
@@ -26,17 +30,6 @@ in {
         '';
 
         enableCompletion = false;
-        initExtraBeforeCompInit = ''
-              export ZINIT_HOME=${cfg.zinitDir}
-              source "${cfg.zinitDir}/zinit.zsh"
-
-              ${optionalString (cfg.plugins != [ ]) ''
-                    ${concatStrings (map (plugin: '' 
-                          ${ optionalString (plugin.ice != [ ]) (iceToStr plugin.ice) }
-                          zinit ${plugin.strategy} ${plugin.name}
-                    '') cfg.plugins)}
-              ''}
-        '';
 
         initExtra = ''
               # Netlify Large media
@@ -47,18 +40,23 @@ in {
               # PNPM setup
               export PATH="$PATH":${config.home.homeDirectory}/.local/share/pnpm/
               export PNPM_HOME="${config.home.homeDirectory}/.local/share/pnpm/"
+
+              # FNM setup
               eval "$(fnm env --use-on-cd)"
         '';
 
         zinit = {
-              enable = true;
-              plugins = [
-                    { name = "zsh-users/zsh-autosuggestions"; ice = [ "wait'1'" "lucid" ]; } # Simple plugin installation
-                    { name = "zsh-users/zsh-syntax-highlighting"; ice = [ "wait'1'" "lucid" ]; } # Simple plugin installation
-                    { name = "chisui/zsh-nix-shell"; } # Simple plugin installation
-                    { name = "sindresorhus/pure"; ice = [ "compile'(pure|async).zsh'" "pick'async.zsh'" "src'pure.zsh'" ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-                    #{ name = "romkatv/powerlevel10k"; ice = [ "depth=1" ]; strategy = "light"; } # Installations with additional options. For the list of options, please refer to Zplug README.
-              ];
+            enable = true;
+            enableSyntaxCompletionsSuggestions = true;
+            plugins = [
+                { name = "chisui/zsh-nix-shell"; } # Simple plugin installation
+                { name = "sindresorhus/pure"; ice = { 
+                  compile = "(pure|async).zsh";
+                  pick = "async.zsh";
+                  src = "pure.zsh";
+                };
+              } # Installations with additional options. For the list of options, please refer to Zplug README.
+            ];
         };
     };
 }
